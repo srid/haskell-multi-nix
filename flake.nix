4e31d3bee68ebece22de2c3d579c1339d297f205
Sridhar Ratnakumar <srid@srid.ca>
2022-11-01 16:52:58 -0400

    Run nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
   outputs = { self, nixpkgs, flake-parts }:
     flake-parts.lib.mkFlake { inherit self; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {self', inputs', pkgs, system, ... }:
+      perSystem = { self', inputs', pkgs, system, ... }:
         let
           overlay = self: super: {
             # Local packages in the repository
-            foo = self.callCabal2nix "foo" ./foo {};
-            bar = self.callCabal2nix "bar" ./bar {};
+            foo = self.callCabal2nix "foo" ./foo { };
+            bar = self.callCabal2nix "bar" ./bar { };
             # TODO: Put any library dependency overrides here
           };
           # Extend the `pkgs.haskellPackages` attrset using an overlay.
@@ -21,7 +21,8 @@
           # the `composeExtensions` (or `composeManyExtensions`) function to
           # merge the overlays.
           haskellPackages' = pkgs.haskellPackages.extend overlay;
-        in {
+        in
+        {
           packages = {
             inherit (haskellPackages') foo bar;
             default = haskellPackages'.bar;
