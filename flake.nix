cdcc2ed0fb889c04f41bd94d397e7ed5ef7da3e7
Sridhar Ratnakumar <srid@srid.ca>
2022-11-01 16:41:54 -0400

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, flake-parts }:
     flake-parts.lib.mkFlake { inherit self; } {
