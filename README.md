4c7e91642a261063edd549b46f0f6234b68b1a57
Hari Amoor <99296920+HariAmoor-professional@users.noreply.github.com>
2022-11-09 15:38:34 -0500

    Use `haskell-flake` (#1)

--- README.md
+++ README.md
@@ -37,8 +37,16 @@ For example, this will compile and run the main executable:
 nix develop -c cabal -- run bar
 ```
 
-## Explanation
+## How it works
+
+### The `nixpkgs` tree
+
+The [`nixpkgs`](https://github.com/srid/haskell-multi-nix/tree/nixpkgs) release tag uses raw functions from nixpkgs.
 
 The Haskell infrastructure in nixpkgs provides a package set (an attrset) called `pkgs.haskellPackages`[^ver]. We add two more packages -- `foo` and `bar` (the local packages) -- to this package set. We do this by using the standard nixpkgs overlay API (specifically `extend`, which was created by the implicit `makeExtensible`) defined in [fixed-points.nix](https://github.com/NixOS/nixpkgs/blob/master/lib/fixed-points.nix). After having added the local packages, the result is a *new* package set, which is no different *in essense* to the original package set (we can also put our dependency overrides in the same, or different, overlay). Note that any package in a package set can depend on any other packages; thus, it becomes possible to make `bar` depend on `foo` (see "build-depends" in `./bar/bar.cabal`) even though they come from the same overlay.
 
 [^ver]: The package set `pkgs.haskellPackages` corresponds to the default GHC version. Non-default GHC versions have their own package sets, for e.g.: `pkgs.haskell.packages.ghc924` is the package set for GHC 9.2.4.
+
+### The `master` tree
+
+The `master` branch uses [haskell-flake](https://github.com/srid/haskell-flake) which abstracts much of what we explained above, such that your flake.nix is as small as possible.
--- flake.nix
+++ flake.nix
@@ -2,43 +2,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = { self, nixpkgs, flake-parts }:
+  outputs = { self, nixpkgs, flake-parts, haskell-flake }:
     flake-parts.lib.mkFlake { inherit self; } {
       systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ haskell-flake.flakeModule ];
       perSystem = { self', inputs', pkgs, system, ... }:
-        let
-          overlay = self: super: {
-            # Local packages in the repository
-            foo = self.callCabal2nix "foo" ./foo { };
-            bar = self.callCabal2nix "bar" ./bar { };
-            # TODO: Put any library dependency overrides here
-          };
-          # Extend the `pkgs.haskellPackages` attrset using an overlay.
-          #
-          # Note that we can also extend the package set using more than one
-          # overlay. To do that we can either chain the `extend` calls or use
-          # the `composeExtensions` (or `composeManyExtensions`) function to
-          # merge the overlays.
-          haskellPackages' = pkgs.haskellPackages.extend overlay;
-        in
         {
-          packages = {
-            inherit (haskellPackages') foo bar;
-            default = haskellPackages'.bar;
+          # Most simple configuration requires only:
+          # haskellProjects.default = { };
+          haskellProjects.default = {
+            # Haskell dependency overrides go here 
+            # overrides = self: super: {
+            # };
+            # hlsCheck = false;
+            # hlintCheck = true;
+            packages = {
+              foo.root = ./foo;
+              bar.root = ./bar;
+            };
           };
-          # This is how we provide a multi-package dev shell in Haskell.
-          # By using the `shellFor` function.
-          devShells.default = haskellPackages'.shellFor {
-            packages = p: [
-              p.foo
-              p.bar
-            ];
-            buildInputs = with haskellPackages'; [
-              ghcid
-              cabal-install
-            ];
-          };
+          packages.default = self'.packages.bar;
         };
     };
 }
